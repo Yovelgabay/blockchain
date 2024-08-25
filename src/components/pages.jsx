b14f413ad6582ea8d6e17148d188fb3a2aaa4b48
Yovel Gabbay <114578333+Yovelgabay@users.noreply.github.com>
2024-08-26 00:08:15 +0300

    changes

--- src/components/pages.jsx
+++ src/components/pages.jsx
@@ -4,7 +4,6 @@ import Create from "./create";
 import Restore from "./restore";
 import Dashboard from "./dashboard";
 import WalletDetails from "./walletDetails";
-import WalletDetails from "./walletDetails";
 
 
 export const pages = {
